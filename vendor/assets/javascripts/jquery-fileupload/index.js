fee3bde3724612396e2a1abdef0222d3b10de15c
nikue <nikueh@gmail.com>
2014-08-24 17:22:23 +0300

    Update index.js

--- vendor/assets/javascripts/jquery-fileupload/index.js
+++ vendor/assets/javascripts/jquery-fileupload/index.js
@@ -4,6 +4,5 @@
 //=require jquery-fileupload/vendor/tmpl
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
-//=require jquery-fileupload/jquery.fileupload-fp
+//=require jquery-fileupload/jquery.fileupload-process
 //=require jquery-fileupload/jquery.fileupload-ui
-//=require jquery-fileupload/locale
